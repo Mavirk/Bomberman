ee190d5faf00e4ca6824123c0a30afe743522ad3
Robert MURDOCH <rmurdoch@c1r4s3.wethinkcode.co.za>
2018-11-06 14:47:40 +0200

    adding model loading code

---
+++ loader/mesh.hpp
@@ -0,0 +1,30 @@
+#ifndef MESH_HPP
+#define MESH_HPP
+
+struct Vertex {
+    glm::vec3 Position;
+    glm::vec3 Normal;
+    glm::vec2 TexCoords;
+};
+
+struct Texture {
+    unsigned int id;
+    string type;
+}; 
+
+class Mesh {
+    public:
+        /*  Mesh Data  */
+        vector<Vertex> vertices;
+        vector<unsigned int> indices;
+        vector<Texture> textures;
+        /*  Functions  */
+        Mesh(vector<Vertex> vertices, vector<unsigned int> indices, vector<Texture> textures);
+        void Draw(Shader shader);
+    private:
+        /*  Render data  */
+        unsigned int VAO, VBO, EBO;
+        /*  Functions    */
+        void setupMesh();
+};
+#endif
